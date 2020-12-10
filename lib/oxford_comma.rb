c3a918ebc04d7ff36a421d0d98293e3b5623224e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-10 11:50:04 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    arr_len = array.length
+    if arr_len == 1
+        return  array[0].to_s
+    elsif arr_len == 2
+        return "#{array[0].to_s} and #{array[1].to_s}"
+    elsif arr_len > 2
+        last_el = array.pop()
+        return array.join(", ").concat(", and #{last_el}")
+    end
 end
