7a090c1eeec7fcd18ae564a30672acf0b7fa9a18
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-17 17:27:27 -0600

    commit tree f20a9d5046eaa6d9af728194b1fb5a4afb0ad0b4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,20 @@
 def oxford_comma(array)
-    arr_len = array.length
-    if arr_len == 1
-        return  array[0].to_s
-    elsif arr_len == 2
-        return "#{array[0].to_s} and #{array[1].to_s}"
-    elsif arr_len > 2
-        last_el = array.pop()
-        return array.join(", ").concat(", and #{last_el}")
+    # arr_len = array.length
+    # if arr_len == 1
+    #     return  array[0].to_s
+    # elsif arr_len == 2
+    #     return "#{array[0].to_s} and #{array[1].to_s}"
+    # elsif arr_len > 2
+    #     last_el = array.pop()
+    #     return array.join(", ").concat(", and #{last_el}")
+    # end
+    #
+    #DJ version
+    if array.length == 2
+        return "#{array.first} and #{array.last}"
+    elsif array.length > 2
+        array[-1].insert(0, "and ")
     end
+    array.join(", ")
 end
+
